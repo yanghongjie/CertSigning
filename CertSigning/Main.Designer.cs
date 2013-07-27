c359a76044a193e5fc9e8609bc7da1bef726b516
yhj <yonghongjie@hotmail.com>
2013-07-27 23:33:58 +0800

    签名有（生成证书和选择已有证书）两种方式

---
+++ CertSigning/Main.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace CertSigning
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btn_Signture = new System.Windows.Forms.Button();
+            this.lvdropitem = new System.Windows.Forms.ListView();
+            this.imagelist = new System.Windows.Forms.ImageList(this.components);
+            this.lbtip = new System.Windows.Forms.Label();
+            this.tb_filepath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.tb_certname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_certpath = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Signture
+            // 
+            this.btn_Signture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Signture.Location = new System.Drawing.Point(399, 221);
+            this.btn_Signture.Name = "btn_Signture";
+            this.btn_Signture.Size = new System.Drawing.Size(83, 27);
+            this.btn_Signture.TabIndex = 0;
+            this.btn_Signture.Text = "签名";
+            this.btn_Signture.UseVisualStyleBackColor = true;
+            this.btn_Signture.Click += new System.EventHandler(this.btn_Signture_Click);
+            // 
+            // lvdropitem
+            // 
+            this.lvdropitem.AllowDrop = true;
+            this.lvdropitem.LargeImageList = this.imagelist;
+            this.lvdropitem.Location = new System.Drawing.Point(94, 26);
+            this.lvdropitem.Margin = new System.Windows.Forms.Padding(0);
+            this.lvdropitem.Name = "lvdropitem";
+            this.lvdropitem.Scrollable = false;
+            this.lvdropitem.Size = new System.Drawing.Size(211, 48);
+            this.lvdropitem.TabIndex = 3;
+            this.lvdropitem.UseCompatibleStateImageBehavior = false;
+            this.lvdropitem.DragDrop += new System.Windows.Forms.DragEventHandler(this.lvdropitem_DragDrop);
+            this.lvdropitem.DragEnter += new System.Windows.Forms.DragEventHandler(this.lvdropitem_DragEnter);
+            // 
+            // imagelist
+            // 
+            this.imagelist.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imagelist.ImageSize = new System.Drawing.Size(32, 32);
+            this.imagelist.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // lbtip
+            // 
+            this.lbtip.AutoSize = true;
+            this.lbtip.BackColor = System.Drawing.Color.White;
+            this.lbtip.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lbtip.Location = new System.Drawing.Point(181, 44);
+            this.lbtip.Name = "lbtip";
+            this.lbtip.Size = new System.Drawing.Size(101, 12);
+            this.lbtip.TabIndex = 10;
+            this.lbtip.Text = "将程序拖拽至框内";
+            // 
+            // tb_filepath
+            // 
+            this.tb_filepath.Location = new System.Drawing.Point(94, 88);
+            this.tb_filepath.Name = "tb_filepath";
+            this.tb_filepath.ReadOnly = true;
+            this.tb_filepath.Size = new System.Drawing.Size(388, 21);
+            this.tb_filepath.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "程序路径：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "当前程序：";
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(407, 123);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_browse.TabIndex = 6;
+            this.btn_browse.Text = "浏览...";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // tb_certname
+            // 
+            this.tb_certname.Location = new System.Drawing.Point(94, 124);
+            this.tb_certname.Name = "tb_certname";
+            this.tb_certname.Size = new System.Drawing.Size(221, 21);
+            this.tb_certname.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "证书名称：";
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(326, 123);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(75, 23);
+            this.btn_generate.TabIndex = 11;
+            this.btn_generate.Text = "生成";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "证书路径：";
+            // 
+            // tb_certpath
+            // 
+            this.tb_certpath.Location = new System.Drawing.Point(94, 162);
+            this.tb_certpath.Name = "tb_certpath";
+            this.tb_certpath.ReadOnly = true;
+            this.tb_certpath.Size = new System.Drawing.Size(388, 21);
+            this.tb_certpath.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(12, 201);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(494, 1);
+            this.textBox2.TabIndex = 12;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 260);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.lbtip);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.tb_certpath);
+            this.Controls.Add(this.tb_filepath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_certname);
+            this.Controls.Add(this.lvdropitem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_Signture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "证书签名";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Signture;
+        private System.Windows.Forms.ListView lvdropitem;
+        private System.Windows.Forms.ImageList imagelist;
+        private System.Windows.Forms.TextBox tb_filepath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_certname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbtip;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_certpath;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CertSigning/Main.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CertSigning
+{
+    public partial class Main : Form
+    {
+        #region 构造函数
+        public Main()
+        {
+            InitializeComponent();
+        } 
+        #endregion
+
+        #region 变量及方法定义
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        private static extern int ExtractIconEx(string lpszFile, int niconIndex, IntPtr[] phiconLarge, IntPtr[] phiconSmall, int nIcons);
+
+        private IntPtr[] largeIcons = new IntPtr[1];
+        #endregion
+
+        #region 控件事件
+        //拖拽完成
+        private void lvdropitem_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+
+            ExtractIconEx(path, 0, largeIcons, new IntPtr[0], 1);
+            imagelist.Images.Clear();
+            imagelist.Images.Add(Icon.FromHandle(largeIcons[0]));
+
+            ListViewItem lvi = new ListViewItem();
+            lvi.ImageIndex = 0;  //listview子项图标索引项  
+
+            this.lvdropitem.Items.Clear();
+            this.lvdropitem.Items.Add(lvi);
+
+            tb_filepath.Text = path;
+        }
+        //拖拽至该控件工作区
+        private void lvdropitem_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+                string extension = System.IO.Path.GetExtension(path);
+                if (extension != ".exe")
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.Move;
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        //生成证书
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            if (tb_certname.Text == string.Empty)
+            {
+                MessageBox.Show("请填写要生成证书的名称", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string path = FolderBrowserDialog();
+            if (string.Empty != path)
+            {
+                string certName = tb_certname.Text;
+                string certpath = Path.Combine(path, certName + ".cer");
+                GenerateCert(certName, certpath);
+                tb_certpath.Text = certpath;
+                MessageBox.Show("生成完毕！", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        //浏览证书
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            string certpath = OpenDialog();
+            if (string.Empty != certpath)
+            {
+                string certName = Path.GetFileNameWithoutExtension(certpath);
+                ImportCert(certName, certpath);
+                tb_certname.Text = certName;
+                tb_certpath.Text = certpath;
+            }
+        }
+        //签名
+        private void btn_Signture_Click(object sender, EventArgs e)
+        {
+            if (tb_filepath.Text == string.Empty)
+            {
+                MessageBox.Show("未找到需要签名的程序", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (tb_certpath.Text == string.Empty)
+            {
+                MessageBox.Show("未找到用来签名的证书", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Signature(tb_certname.Text, tb_filepath.Text);
+
+            MessageBox.Show("签名完毕！\r\n提示：重新以管理员身份运行该程序可查看签名效果", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } 
+        #endregion
+
+        #region 私有方法
+        /// <summary>
+        /// 证书生成到
+        /// </summary>
+        private string FolderBrowserDialog()
+        {
+            string localFilePath = string.Empty;
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            // 设置根在桌面
+            fbd.RootFolder = System.Environment.SpecialFolder.Desktop;
+            // 设置当前选择的路径
+            fbd.SelectedPath = "C:";
+            // 允许在对话框中包括一个新建目录的按钮
+            fbd.ShowNewFolderButton = true;
+            // 设置对话框的说明信息
+            fbd.Description = "请选择证书输出目录";
+            // 打开
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                localFilePath = fbd.SelectedPath;
+            }
+
+            return localFilePath;
+        }
+        /// <summary>
+        /// 选择证书
+        /// </summary>
+        /// <returns></returns>
+        private string OpenDialog()
+        {
+            string localFilePath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "证书文件(.cer)|*.cer";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                localFilePath = openFileDialog.FileName;
+            }
+            return localFilePath;
+        }
+        /// <summary>
+        /// 生成证书
+        /// </summary>
+        /// <param name="certName">证书名称</param>
+        /// <param name="certPath">证书生成后导出路径</param>
+        private void GenerateCert(string certName, string certPath)
+        {
+            //证书创建工具，根据vs版本不同可能位置不一样，下面是vs2012位置，http://msdn.microsoft.com/zh-cn/library/bfsktky3(v=vs.100).aspx
+            const string MakeCert = "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v7.1A\\bin\\makecert.exe";
+            //证书生成成功后默认导出到当前项目根目录
+            //string certPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "demo.cer");
+            //生成证书命令行，生成在当前用户的个人证书存储区
+            string arguments = string.Format("-r -pe -n \"CN={0}\" -a sha1 -b 01/01/2012 -e 01/01/2040 -sky exchange \"{1}\" -ss my", certName, certPath);
+            //生成证书
+            ProcessStartInfo pi = new ProcessStartInfo(MakeCert, arguments);
+            //隐藏
+            pi.WindowStyle = ProcessWindowStyle.Hidden;
+            //开始生成证书
+            Process p = Process.Start(pi);
+            p.WaitForExit();
+            //找到生成的证书
+            X509Certificate2 cert = new X509Certificate2(certPath);
+            if (cert != null)
+            {
+                //查找个人证书区是否已经存在该证书，不存在则添加
+                var storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                storeMy.Open(OpenFlags.ReadWrite);
+                bool MyIsExist = storeMy.Certificates.Find(X509FindType.FindByIssuerName, certName, true).Count == 0;
+                if (MyIsExist)//(!storeMy.Certificates.Contains(cert))
+                {
+                    storeMy.Add(cert);
+                }
+                //查找受信任的根证书颁发机构证书区是否已经存在该证书，不存在则添加
+                var storeRoot = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+                storeRoot.Open(OpenFlags.ReadWrite);
+                bool RootIsExist = storeRoot.Certificates.Find(X509FindType.FindByIssuerName, certName, true).Count == 0;
+                if (RootIsExist)//(!storeRoot.Certificates.Contains(cert))
+                {
+                    storeRoot.Add(cert);
+                }
+            }
+        }
+        /// <summary>
+        /// 导入证书
+        /// </summary>
+        /// <param name="certName">证书名称</param>
+        /// <param name="certPath">证书路径</param>
+        private void ImportCert(string certName, string certPath)
+        {
+            //找到生成的证书
+            X509Certificate2 cert = new X509Certificate2(certPath);
+            if (cert != null)
+            {
+                //查找个人证书区是否已经存在该证书，不存在则添加
+                var storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                storeMy.Open(OpenFlags.ReadWrite);
+                bool MyIsExist = storeMy.Certificates.Find(X509FindType.FindByIssuerName, certName, true).Count == 0;
+                if (MyIsExist)//(!storeMy.Certificates.Contains(cert))
+                {
+                    storeMy.Add(cert);
+                }
+                //查找受信任的根证书颁发机构证书区是否已经存在该证书，不存在则添加
+                var storeRoot = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+                storeRoot.Open(OpenFlags.ReadWrite);
+                bool RootIsExist = storeRoot.Certificates.Find(X509FindType.FindByIssuerName, certName, true).Count == 0;
+                if (RootIsExist)//(!storeRoot.Certificates.Contains(cert))
+                {
+                    storeRoot.Add(cert);
+                }
+            }
+        }
+        /// <summary>
+        /// 签名
+        /// </summary>
+        /// <param name="certName">证书名称</param>
+        /// <param name="ProgramPath">需要签名的程序路径</param>
+        private void Signature(string certName, string ProgramPath)
+        {
+            //签名工具 http://msdn.microsoft.com/zh-cn/library/8s9b9yaz(v=vs.100).aspx
+            const string Signtool = "C:\\Program Files (x86)\\Windows Kits\\8.0\\bin\\x86\\signtool.exe";
+            //签名命令行
+            string arguments = string.Format("sign /a /n \"{0}\" \"{1}\"", certName, ProgramPath);
+            //签名
+            ProcessStartInfo pi = new ProcessStartInfo(Signtool, arguments);
+            //隐藏
+            pi.WindowStyle = ProcessWindowStyle.Hidden;
+            //开始签名
+            Process p = Process.Start(pi);
+            p.WaitForExit();
+        } 
+        #endregion
+    }
+}
