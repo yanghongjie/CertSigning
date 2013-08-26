b87cfb84256b6b5e4101676ed28d7ac1a8370542
yhj <yonghongjie@hotmail.com>
2013-08-26 13:45:30 +0800

    修改

--- CertSigning/Main.Designer.cs
+++ CertSigning/Main.Designer.cs
@@ -196,6 +196,7 @@
             this.Controls.Add(this.btn_Signture);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "证书签名";
